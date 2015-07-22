6cb303b3260a0d24f6809c5e71c828c294cfa524
Joe Duhamel <joe.duhamel@geneticfinance.com>
2015-07-22 14:26:10 -0700

    Filled out  cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,39 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+# Clear the variables. then return a list with the 4 functions, (set,get, setinv, getinv. )
 makeCacheMatrix <- function(x = matrix()) {
-
+	inv = NULL
+	set = function(setVal) {
+		x <<- setVal
+		inv <<- NULL
+	}
+	get = function() x
+	setinv = function (inverse) inv <<- inverse
+	getinv = function() inv
+	list(set=set, get=get, setinv=setinv,getinv=getinv)
 }
 
 
 ## Write a short comment describing this function
+## Basically we just pull off of setinv if it's set.  and store the inverse. 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinv()
+
+ 
+    if (!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+
+    # The inverse is not yet calculated, so we calculate it
+    data <- x$get()
+    inv <- solve(data, ...)
+
+    # Cache the inverse
+    x$setinv(inv)
+
+    # Return it
+    inv
 }
